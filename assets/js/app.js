2dcd7bda1fa6a07b3058ca0674e6b899f422f4a8
Derick Ruiz <derickiscool@gmail.com>
2016-07-04 15:44:28 +0700

    Getting some mock data up and going to start styling the user interface.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,40 @@
 
     var app = document.getElementById("app");
 
+    var CreateTimerButton = function () {
+        return m("button", {class: "Button"}, "create a timer");
+    };
+
+    var Home = {
+        controller: function () {
+            var timers = [{
+                timerName: "web dev"
+            },
+            {
+                timerName: "another timer"
+            }];
+
+            return {
+                timers: timers
+            }
+        },
+        view: function (controller) {
+            if (controller.timers) {
+                return m("div", [
+                    controller.timers.map(function (timer, index) {
+                        return m("div", timer.timerName);
+                    }),
+                    CreateTimerButton()
+                ]);
+            }
+
+        }
+    };
+
+    m.route.mode = "hash";
+
     m.route(app, "/", {
-        "/": home
+        "/": Home
     });
 
-    var home = {
-
-    };
-    
 })();
